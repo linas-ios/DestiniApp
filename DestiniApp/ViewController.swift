efab6b123aade3726e3784ea02aed8017031564e
linas-ios <111570883+linas-ios@users.noreply.github.com>
2022-11-23 21:48:43 +0100

    update storyLabel and buttons titles

--- DestiniApp/ViewController.swift
+++ DestiniApp/ViewController.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  //MARK: Properties
+  
+  let story0 = "You see a fork in the road"
+  let choise1 = "Take a left"
+  let choise2 = "Take a right"
+  
   @IBOutlet weak var storyLabel: UILabel!
   @IBOutlet weak var choiceOneButton: UIButton!
   @IBOutlet weak var choiceTwoButton: UIButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    storyLabel.text = story0
   }
 
   @IBAction func choiceMade(_ sender: UIButton) {
