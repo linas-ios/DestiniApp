4eb0b28bf7013ce6c5be16aa1ea7d61ddcc2bd2f
linas-ios <111570883+linas-ios@users.noreply.github.com>
2022-11-23 21:31:57 +0100

    add background, add stackView

---
+++ DestiniApp/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  DestiniApp
+//
+//  Created by Linas Nutautas on 23/11/2022.
+//
+
+import Foundation
---
+++ DestiniApp/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  DestiniApp
+//
+//  Created by Linas Nutautas on 23/11/2022.
+//
+
+import Foundation
