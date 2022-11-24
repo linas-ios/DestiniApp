3d8be52a847d41c2697c74e87ad2793f86b92222
linas-ios <111570883+linas-ios@users.noreply.github.com>
2022-11-24 22:12:15 +0100

    add 'StoryBrain' methods

--- DestiniApp/Controller/ViewController.swift
+++ DestiniApp/Controller/ViewController.swift
@@ -9,13 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  //MARK: Properties
-  let stories = [
-    Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
-    Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
-    Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
-]
-  var storyNumber = 0
+  var storyBrain = StoryBrain()
   
   @IBOutlet weak var storyLabel: UILabel!
   @IBOutlet weak var choiceOneButton: UIButton!
@@ -24,26 +18,21 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     updateUI()
-
-  }
-
-  @IBAction func choiceMade(_ sender: UIButton) {
-    let userChoise = sender.currentTitle!
     
-    if userChoise == stories[storyNumber].choice1 {
-      storyNumber += 1
-    } else if userChoise == stories[storyNumber].choice2 {
-      storyNumber += 2
-    }
-    updateUI()
   }
   
+  @IBAction func choiceMade(_ sender: UIButton) {
+    let userChoises = sender.currentTitle!
+    storyBrain.nextStory(userChoice: userChoises)
+    
+    
+    
+  }
   //MARK: Methods
-  
   func updateUI() {
-    storyLabel.text = stories[storyNumber].title
-    choiceOneButton.setTitle(stories[storyNumber].choice1, for: .normal)
-    choiceTwoButton.setTitle(stories[storyNumber].choice2, for: .normal)
+    storyLabel.text = storyBrain.getStory()
+    choiceOneButton.setTitle(storyBrain.choiceOneStory(), for: .normal)
+    choiceTwoButton.setTitle(storyBrain.choiceTwoStory(), for: .normal)
   }
   
 }
--- DestiniApp/Model/StoryBrain.swift
+++ DestiniApp/Model/StoryBrain.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct StoryBrain {
+  let stories = [
+    Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
+    Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
+    Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
+  ]
+  var storyNumber = 0
+  
+  mutating func nextStory(userChoice: String) {
+    if userChoice == stories[storyNumber].choice1 {
+      storyNumber += 1
+    } else if userChoice == stories[storyNumber].choice2 {
+      storyNumber += 2
+    }
+  }
+  
+  //MARK: Methods
+  
+  func getStory() -> String {
+    return stories[storyNumber].title
+  }
+  
+  func choiceOneStory() -> String {
+    return stories[storyNumber].choice1
+  }
+  
+  func choiceTwoStory() -> String {
+    return stories[storyNumber].choice2
+  }
+  
+}
